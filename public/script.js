46b6cf6ddffe84baf14276afcd6ec705522c10b5
BuildTools <unconfigured@null.spigotmc.org>
2022-08-24 06:43:03 -0300

    Sprites and Game

--- public/script.js
+++ public/script.js
@@ -1,4 +1,11 @@
 (function () {
+  window.addEventListener("beforeunload", (evento) => {
+    if (true) {
+      evento.preventDefault();
+      evento.returnValue = "";
+      return "";
+    }
+  });
   const app = document.querySelector(".app");
   const socket = io();
   socket.on("serverestart", function () {
@@ -39,7 +46,7 @@
       uname = username;
       app.querySelector(".join-screen").classList.remove("active");
       app.querySelector(".chat-screen").classList.add("active");
-      app.querySelector("#game").classList.add("active");
+      app.querySelector("#pintura").classList.add("active");
       // requestAndShowPermission();
     });
   app
@@ -105,14 +112,13 @@
     } else if (type == "other") {
       let el = document.createElement("div");
       el.setAttribute("class", "message other-message");
-      console.log(message.last);
-      if(message.last === true){
+      if (message.last === true) {
         el.innerHTML = `
         <div>
             <div class="text">${message.text}</div>
         </div>
         `;
-      }else{
+      } else {
         el.innerHTML = `
         <div>
             <div class="name">${message.username}</div>
@@ -150,10 +156,12 @@
   //   }
   // }
 
+  var canvasWidth = 1000;
+  var canvasHeight = 600;
   var canvas = $("#paint");
   canvas.attr({
-    width: 380,
-    height: 435,
+    width: canvasWidth,
+    height: canvasHeight,
     // width: 380*$("#paint-container")[0].offsetHeight/435,
     // height: 800*$("#paint-container")[0].offsetHeight/810,
   });
@@ -168,6 +176,7 @@
     linewidth: 1,
     typetrazo: "round",
   };
+  let eraserColor;
   var goDraw = true;
   var drawing = false;
   var trazosActive = false;
@@ -192,7 +201,12 @@
 
   socket.on("drawing", onDrawingEvent);
   socket.on("clean", onCleanEvent);
-  
+
+  function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255) throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+  }
+
   function drawLine(
     x0,
     y0,
@@ -204,26 +218,22 @@
     typeTrazo,
     emit
   ) {
-    console.log(x0, y0, x1, y1, color,opacity, lineWidth, typeTrazo, emit);
+    console.log(x0, y0, x1, y1, color, opacity, lineWidth, typeTrazo, emit);
     ctx.beginPath();
     if (eyedropperIsActive == true) {
       var pxData = ctx.getImageData(x0, y0, 1, 1);
-      eyedropperIsActive = false;
-      colorUpdate(
-        "rgb(" +
-          pxData.data[0] +
-          "," +
-          pxData.data[1] +
-          "," +
-          pxData.data[2] +
-          ")"
-      );
+      var hex =
+        "#" +
+        (
+          "000000" + rgbToHex(pxData.data[0], pxData.data[1], pxData.data[2])
+        ).slice(-6);
+      colorUpdate(hex);
       return;
     }
-    switch (typeTrazo) {
-      case "spray":
+    const TYPE_TRAZOS = {
+      spray: () => {
         ctx.beginPath();
-        ctx.fillStyle = color;
+        ctx.fillStyle = `${color}${opacity}`;
         for (var i = 10; i--; ) {
           ctx.rect(
             x0 + Math.random() * lineWidth - 10,
@@ -250,8 +260,8 @@
           lW: lineWidth,
           tT: typeTrazo,
         });
-        break;
-      case "fill":
+      },
+      fill: () => {
         ctx.beginPath();
         ctx.rect(0, 0, canvas.width(), canvas.height());
         ctx.fillStyle = `${color}${opacity}`;
@@ -273,35 +283,118 @@
           lW: lineWidth,
           tT: typeTrazo,
         });
-        break;
-      default:
-        ctx.beginPath();
-        ctx.moveTo(x0, y0);
-        ctx.lineTo(x1, y1);
-        ctx.strokeStyle = `${color}${opacity}`;
-        ctx.lineWidth = lineWidth;
-        ctx.lineCap = typeTrazo;
-        ctx.stroke();
-        ctx.closePath();
+      },
+    };
+    const TYPE_TRAZOS_DEFAULT = () => {
+      ctx.beginPath();
+      ctx.moveTo(x0, y0);
+      ctx.lineTo(x1, y1);
+      ctx.strokeStyle = `${color}${opacity}`;
+      ctx.lineWidth = lineWidth;
+      ctx.lineCap = typeTrazo;
+      ctx.stroke();
+      ctx.closePath();
 
-        if (!emit) {
-          return;
-        }
-        var w = canvas.width();
-        var h = canvas.height();
+      if (!emit) {
+        return;
+      }
+      var w = canvas.width();
+      var h = canvas.height();
 
-        socket.emit("drawing", {
-          x0: x0 / w,
-          y0: y0 / h,
-          x1: x1 / w,
-          y1: y1 / h,
-          color: color,
-          opacity: opacity,
-          lW: lineWidth,
-          tT: typeTrazo,
-        });
-        break;
-    }
+      socket.emit("drawing", {
+        x0: x0 / w,
+        y0: y0 / h,
+        x1: x1 / w,
+        y1: y1 / h,
+        color: color,
+        opacity: opacity,
+        lW: lineWidth,
+        tT: typeTrazo,
+      });
+    };
+    TYPE_TRAZOS[typeTrazo] ? TYPE_TRAZOS[typeTrazo]() : TYPE_TRAZOS_DEFAULT();
+    // switch (typeTrazo) {
+    //   case "spray":
+    //     ctx.beginPath();
+    //     ctx.fillStyle = `${color}${opacity}`;
+    //     for (var i = 10; i--; ) {
+    //       ctx.rect(
+    //         x0 + Math.random() * lineWidth - 10,
+    //         y1 + Math.random() * lineWidth - 10,
+    //         1,
+    //         1
+    //       );
+    //       ctx.fill();
+    //     }
+    //     ctx.closePath();
+    //     if (!emit) {
+    //       return;
+    //     }
+    //     var w = canvas.width();
+    //     var h = canvas.height();
+
+    //     socket.emit("drawing", {
+    //       x0: x0 / w,
+    //       y0: y0 / h,
+    //       x1: x1 / w,
+    //       y1: y1 / h,
+    //       color: color,
+    //       opacity: opacity,
+    //       lW: lineWidth,
+    //       tT: typeTrazo,
+    //     });
+    //     break;
+    //   case "fill":
+    //     ctx.beginPath();
+    //     ctx.rect(0, 0, canvas.width(), canvas.height());
+    //     ctx.fillStyle = `${color}${opacity}`;
+    //     ctx.fill();
+    //     ctx.closePath();
+    //     if (!emit) {
+    //       return;
+    //     }
+    //     var w = canvas.width();
+    //     var h = canvas.height();
+
+    //     socket.emit("drawing", {
+    //       x0: w,
+    //       y0: h,
+    //       x1: w,
+    //       y1: h,
+    //       color: color,
+    //       opacity: opacity,
+    //       lW: lineWidth,
+    //       tT: typeTrazo,
+    //     });
+    //     break;
+    //   default:
+    //     ctx.beginPath();
+    //     ctx.moveTo(x0, y0);
+    //     ctx.lineTo(x1, y1);
+    //     ctx.strokeStyle = `${color}${opacity}`;
+    //     ctx.lineWidth = lineWidth;
+    //     ctx.lineCap = typeTrazo;
+    //     ctx.stroke();
+    //     ctx.closePath();
+
+    //     if (!emit) {
+    //       return;
+    //     }
+    //     var w = canvas.width();
+    //     var h = canvas.height();
+
+    //     socket.emit("drawing", {
+    //       x0: x0 / w,
+    //       y0: y0 / h,
+    //       x1: x1 / w,
+    //       y1: y1 / h,
+    //       color: color,
+    //       opacity: opacity,
+    //       lW: lineWidth,
+    //       tT: typeTrazo,
+    //     });
+    //     break;
+    // }
   }
 
   function onMouseDown(e) {
@@ -365,12 +458,23 @@
     switch (e.target.className.split(" ")[1]) {
       case "eraser":
         current.typetrazo = "round";
-        current.color = "#ffffff";
+        current.color = eraserColor;
         break;
       case "fill":
+        eraserColor = current.color;
         var w = canvas.width();
         var h = canvas.height();
-        drawLine(w, h, w, h, current.color, current.opacity, current.linewidth, "fill", true);
+        drawLine(
+          w,
+          h,
+          w,
+          h,
+          current.color,
+          current.opacity,
+          current.linewidth,
+          "fill",
+          true
+        );
         break;
       case "eyedropper":
         if (eyedropperIsActive == true) {
@@ -430,6 +534,9 @@
     ctx.clearRect(0, 0, canvas.width(), canvas.height());
     socket.emit("clean");
   });
+  $("#undo-btn").click(function () {
+    app.querySelector("#game").classList.toggle("active");
+  });
 
   $("#zoomIn").click(function () {
     goDraw = false;
@@ -437,4 +544,247 @@
   $("#zoomOut").click(function () {
     goDraw = true;
   });
+
+  // Juego
+  var ASSET_URL = "assets/world/";
+
+  var game = new Phaser.Game(canvasWidth, canvasHeight, Phaser.AUTO, "game", {
+    preload: preload,
+    create: create,
+    update: GameLoop,
+  }, true);
+
+  var WORLD_SIZE = { w: canvasWidth, h: canvasHeight };
+
+  var water_tiles = [];
+  var bullet_array = [];
+  var other_players = {};
+
+  var player = {
+    sprite: null, //Will hold the sprite when it's created
+    speed_x: 0, // This is the speed it's currently moving at
+    speed_y: 0,
+    speed: 0.5, // This is the parameter for how fast it should move
+    friction: 0.95,
+    shot: false,
+    update: function () {
+      // Leap rotation towards mouse
+      var dx = game.input.mousePointer.x + game.camera.x - this.sprite.x;
+      var dy = game.input.mousePointer.y + game.camera.y - this.sprite.y;
+      var angle = Math.atan2(dy, dx) - Math.PI / 2;
+      var dir = (angle - this.sprite.rotation) / (Math.PI * 2);
+      dir -= Math.round(dir);
+      dir = dir * Math.PI * 2;
+      this.sprite.rotation += dir * 0.1;
+
+      // Move forward
+      if (
+        game.input.keyboard.isDown(Phaser.Keyboard.W) ||
+        game.input.keyboard.isDown(Phaser.Keyboard.UP)
+        // game.input.touch.
+      ) {
+        this.speed_x +=
+          Math.cos(this.sprite.rotation + Math.PI / 2) * this.speed;
+        this.speed_y +=
+          Math.sin(this.sprite.rotation + Math.PI / 2) * this.speed;
+      }
+
+      this.sprite.x += this.speed_x;
+      this.sprite.y += this.speed_y;
+
+      this.speed_x *= this.friction;
+      this.speed_y *= this.friction;
+
+      // Shoot bullet
+      if (game.input.activePointer.leftButton.isDown && !this.shot) {
+        var speed_x = Math.cos(this.sprite.rotation + Math.PI / 2) * 20;
+        var speed_y = Math.sin(this.sprite.rotation + Math.PI / 2) * 20;
+        this.shot = true;
+        // Tell the server we shot a bullet
+        socket.emit("shoot-bullet", {
+          x: this.sprite.x,
+          y: this.sprite.y,
+          angle: this.sprite.rotation,
+          speed_x: speed_x,
+          speed_y: speed_y,
+        });
+      }
+      if (!game.input.activePointer.leftButton.isDown) this.shot = false;
+
+      // To make player flash when they are hit, set player.spite.alpha = 0
+      if (this.sprite.alpha < 1) {
+        this.sprite.alpha += (1 - this.sprite.alpha) * 0.16;
+      } else {
+        this.sprite.alpha = 1;
+      }
+
+      // Tell the server we've moved
+      socket.emit("move-player", {
+        x: this.sprite.x,
+        y: this.sprite.y,
+        angle: this.sprite.rotation,
+      });
+    },
+  };
+
+  function CreateShip(type, x, y, angle) {
+    // type is an int that can be between 1 and 6 inclusive
+    // returns the sprite just created
+    var sprite = game.add.sprite(x, y, "ship" + String(type) + "_1");
+    sprite.rotation = angle;
+    sprite.anchor.setTo(0.5, 0.5);
+    return sprite;
+  }
+
+  function preload() {
+    game.load.crossOrigin = "Anonymous";
+
+    // Load all the ships
+    for (var i = 1; i <= 6; i++) {
+      game.load.image(
+        "ship" + String(i) + "_1",
+        ASSET_URL + "ship" + String(i) + "_1.png"
+      );
+      game.load.image(
+        "ship" + String(i) + "_2",
+        ASSET_URL + "ship" + String(i) + "_2.png"
+      );
+      game.load.image(
+        "ship" + String(i) + "_3",
+        ASSET_URL + "ship" + String(i) + "_3.png"
+      );
+      game.load.image(
+        "ship" + String(i) + "_4",
+        ASSET_URL + "ship" + String(i) + "_4.png"
+      );
+    }
+
+    game.load.image("bullet", ASSET_URL + "cannon_ball.png");
+    game.load.image("caracter", "assets/img_the_scream.jpg");
+  }
+
+  function create() {
+    game.stage.disableVisibilityChange = true;
+
+    // Create player
+    var player_ship_type = String(1);
+    player.sprite = game.add.sprite(
+      (Math.random() * WORLD_SIZE.w) / 2 + WORLD_SIZE.w / 2,
+      (Math.random() * WORLD_SIZE.h) / 2 + WORLD_SIZE.h / 2,
+      "ship" + player_ship_type + "_4"
+    );
+    player.sprite.anchor.setTo(0.5, 0.5);
+
+    game.world.setBounds(0, 0, WORLD_SIZE.w, WORLD_SIZE.h);
+
+    game.camera.x = player.sprite.x - canvasWidth / 2;
+    game.camera.y = player.sprite.y - canvasHeight / 2;
+
+    socket.emit("new-player", {
+      x: player.sprite.x,
+      y: player.sprite.y,
+      angle: player.sprite.rotation,
+      type: 1,
+    });
+    // Listen for other players connecting
+    socket.on("update-players", function (players_data) {
+      var players_found = {};
+      // Loop over all the player data received
+      for (var id in players_data) {
+        // If the player hasn't been created yet
+        if (other_players[id] == undefined && id != socket.id) {
+          // Make sure you don't create yourself
+          var data = players_data[id];
+          var p = CreateShip(data.type, data.x, data.y, data.angle);
+          other_players[id] = p;
+          console.log("Created new player at (" + data.x + ", " + data.y + ")");
+        }
+        players_found[id] = true;
+
+        // Update positions of other players
+        if (id != socket.id) {
+          other_players[id].target_x = players_data[id].x; // Update target, not actual position, so we can interpolate
+          other_players[id].target_y = players_data[id].y;
+          other_players[id].target_rotation = players_data[id].angle;
+        }
+      }
+      // Check if a player is missing and delete them
+      for (var id in other_players) {
+        if (!players_found[id]) {
+          other_players[id].destroy();
+          delete other_players[id];
+        }
+      }
+    });
+
+    // Listen for bullet update events
+    socket.on("bullets-update", function (server_bullet_array) {
+      // If there's not enough bullets on the client, create them
+      for (var i = 0; i < server_bullet_array.length; i++) {
+        if (bullet_array[i] == undefined) {
+          bullet_array[i] = game.add.sprite(
+            server_bullet_array[i].x,
+            server_bullet_array[i].y,
+            "bullet"
+          );
+        } else {
+          //Otherwise, just update it!
+          bullet_array[i].x = server_bullet_array[i].x;
+          bullet_array[i].y = server_bullet_array[i].y;
+        }
+      }
+      // Otherwise if there's too many, delete the extra
+      for (var i = server_bullet_array.length; i < bullet_array.length; i++) {
+        bullet_array[i].destroy();
+        bullet_array.splice(i, 1);
+        i--;
+      }
+    });
+
+    // Listen for any player hit events and make that player flash
+    socket.on("player-hit", function (id) {
+      if (id == socket.id) {
+        //If this is you
+        player.sprite.alpha = 0;
+      } else {
+        // Find the right player
+        other_players[id].alpha = 0;
+      }
+    });
+  }
+
+  function GameLoop() {
+    player.update();
+
+    // Move camera with player
+    var camera_x = player.sprite.x - canvasWidth/ 2;
+    var camera_y = player.sprite.y - canvasHeight / 2;
+    game.camera.x += (camera_x - game.camera.x) * 0.08;
+    game.camera.y += (camera_y - game.camera.y) * 0.08;
+
+    // Each player is responsible for bringing their alpha back up on their own client
+    // Make sure other players flash back to alpha = 1 when they're hit
+    for (var id in other_players) {
+      if (other_players[id].alpha < 1) {
+        other_players[id].alpha += (1 - other_players[id].alpha) * 0.16;
+      } else {
+        other_players[id].alpha = 1;
+      }
+    }
+
+    // Interpolate all players to where they should be
+    for (var id in other_players) {
+      var p = other_players[id];
+      if (p.target_x != undefined) {
+        p.x += (p.target_x - p.x) * 0.16;
+        p.y += (p.target_y - p.y) * 0.16;
+        // Interpolate angle while avoiding the positive/negative issue
+        var angle = p.target_rotation;
+        var dir = (angle - p.rotation) / (Math.PI * 2);
+        dir -= Math.round(dir);
+        dir = dir * Math.PI * 2;
+        p.rotation += dir * 0.16;
+      }
+    }
+  }
 })();
--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const hbs = require("hbs");
 
 var isIntervalInProgress = false;
 let users = [];
-let lastMessage; 
+let lastMessage;
+var players = {}; //Keeps a table of all players, the key is the socket id
+var bullet_array = []; // Keeps track of all the bullets to update them on the server
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
@@ -25,6 +27,41 @@ app.get("/admin", (req, res) => {
 });
 
 io.on("connection", function (socket) {
+  var address = socket.handshake.address;
+  var json = JSON.stringify(lookup(address));
+  console.log("--> New connection from: " + address);
+  fs.appendFile("ip.txt", JSON.stringify(address), function (err) {
+    if (err) console.log(err);
+  });
+  fs.appendFile("ip.txt", json, function (err) {
+    if (err) console.log(err);
+  });
+  // Listen for a new player trying to connect
+  socket.on("new-player", function (state) {
+    console.log("New player joined with state:", state);
+    players[socket.id] = state;
+    // Broadcast a signal to everyone containing the updated players list
+    io.emit("update-players", players);
+  });
+  // Listen for move events and tell all other clients that something has moved
+  socket.on("move-player", function (position_data) {
+    if (players[socket.id] == undefined) return; // Happens if the server restarts and a client is still connected
+    players[socket.id].x = position_data.x;
+    players[socket.id].y = position_data.y;
+    players[socket.id].angle = position_data.angle;
+    io.emit("update-players", players);
+  });
+
+  // Listen for shoot-bullet events and add it to our bullet array
+  socket.on("shoot-bullet", function (data) {
+    if (players[socket.id] == undefined) return;
+    var new_bullet = data;
+    data.owner_id = socket.id; // Attach id of the player to the bullet
+    if (Math.abs(data.speed_x) > 20 || Math.abs(data.speed_y) > 20) {
+      console.log("Player", socket.id, "is cheating!");
+    }
+    bullet_array.push(new_bullet);
+  });
   if (!isIntervalInProgress) {
     isIntervalInProgress = true;
     setInterval(function () {
@@ -32,18 +69,8 @@ io.on("connection", function (socket) {
       socket.emit("board", users);
     }, 2000);
   }
-  var address = socket.handshake.address;
-  var json = JSON.stringify(lookup(address));
-  console.log("--> New connection from: " + address);
-  //console.log(lookup(address)); // location of the user
-  fs.appendFile("ip.txt", JSON.stringify(address), function (err) {
-    if (err) console.log(err);
-  });
-  fs.appendFile("ip.txt", json, function (err) {
-    if (err) console.log(err);
-  });
   socket.on("newuser", function (username) {
-    if (username.length == 0 || username.length >= 14) {
+    if (username.length === 0 || username.length >= 14) {
       socket.emit("serverestart");
       return;
     }
@@ -74,7 +101,7 @@ io.on("connection", function (socket) {
     }
     try {
       let usuario = users.find((e) => e.id === socket.id);
-      if(lastMessage === usuario.id){
+      if (lastMessage === usuario.id) {
         message.last = true;
       }
       usuario.messages++;
@@ -139,9 +166,45 @@ io.on("connection", function (socket) {
         users.findIndex((e) => e.id === socket.id),
         1
       );
+      delete players[socket.id];
+      io.emit("update-players", players);
     } catch (err) {
       console.log(`--> Usuario no autenticado fue desconectado`);
     }
   });
 });
+function ServerGameLoop() {
+  for (var i = 0; i < bullet_array.length; i++) {
+    var bullet = bullet_array[i];
+    bullet.x += bullet.speed_x;
+    bullet.y += bullet.speed_y;
+
+    // Check if this bullet is close enough to hit any player
+    for (var id in players) {
+      if (bullet.owner_id != id) {
+        // And your own bullet shouldn't kill you
+        var dx = players[id].x - bullet.x;
+        var dy = players[id].y - bullet.y;
+        var dist = Math.sqrt(dx * dx + dy * dy);
+        if (dist < 70) {
+          io.emit("player-hit", id); // Tell everyone this player got hit
+        }
+      }
+    }
+
+    // Remove if it goes too far off screen
+    if (
+      bullet.x < -10 ||
+      bullet.x > 1000 ||
+      bullet.y < -10 ||
+      bullet.y > 1000
+    ) {
+      bullet_array.splice(i, 1);
+      i--;
+    }
+  }
+  // Tell everyone where all the bullets are by sending the whole array
+  io.emit("bullets-update", bullet_array);
+}
+setInterval(ServerGameLoop, 16);
 server.listen(7777);
